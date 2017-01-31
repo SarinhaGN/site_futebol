db466ccd0685144a09ae2f06ad706a72e5135513
SarinhaGN <sarah.gomes80@hotmail.com>
2017-01-31 11:39:14 -0200

    subistituicao do bootstrap fisico para cdn, adicao de plugin plyr e arquivo para legendas. Criacao e conclusao do projeto mobile.

--- js/efeitos.js
+++ js/efeitos.js
@@ -22,9 +22,54 @@ $(document).ready(function(){
 	
 	$(".thumbnails").owlCarousel({
  
-      autoPlay: 2000,
+      autoPlay: 4000,
       items : 4
  
   	});
+
+	var owl = $(".thumbnails").data('owlCarousel');
+
+	$('#btn-news-prev').on("click", function(){
+
+		owl.prev();
+
+	});
+
+	$('#btn-news-next').on("click", function(event){
+
+		owl.next();
+
+	});
+
+	$("#page-up").on("click", function(){
+
+		$("body").animate({
+			scrollTop:0
+
+		}, 1000);
+
+		event.preventDefault();
+
+	});
+
+	$("#btn-bars").on("click", function(){
+
+		$("header").toggleClass("open-menu");
+		/*toggleClass = se tiver a classe retire, se nao tiver coloque*/
+
+	});
+
+	$("#menu-mobile-mask, .btn-close").on("click", function(){
+
+		$("header").removeClass("open-menu");
+
+	});
+
+	$("#btn-search").on("click", function(){
+
+		$("header").toggleClass("open-search");
+		$("#input-search-mobile").focus();
+
+	});
 
 });
